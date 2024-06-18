d07ffb2c02c7f33702fc31f4c1287f7da015ea29
lxy <577086410@qq.com>
2024-06-18 16:03:26 +0800

    Finish: 01_Background/Source_Code/00add_P.cu ToDo: What will happend if array size too large? 	And how to handle that?
    Fix: Divide N by 1024.0 with a float type.  New branch.
     Changes to be committed:
    	modified:   00add_P.cu
     Changes not staged for commit:
    	modified:   00add_P.cu

--- 01_Background/Source_code/00add_P.cu
+++ 01_Background/Source_code/00add_P.cu
@@ -1,37 +1,67 @@
 #include <assert.h>
 #include <stdio.h>
 
+__global__ void add_kernel(int* device_a, int* device_b, int* device_result, int N){
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if(idx < N && idx >= 0)
+        device_result[idx] = device_a[idx] + device_b[idx];
+}
+
 // Do the add vector operation
 int* add(int* a, int* b, int* result, int N) {
-    // -:YOUR CODE HERE:-
+    int ARRAY_BYTES = N * sizeof(int);
+    int  *device_a, *device_b, *device_result;
+    cudaMalloc(&device_a, ARRAY_BYTES);
+    cudaMalloc(&device_b, ARRAY_BYTES);
+    cudaMalloc(&device_result, ARRAY_BYTES);
+    cudaMemcpy(device_a, a, ARRAY_BYTES, cudaMemcpyHostToDevice);
+    cudaMemcpy(device_b, b, ARRAY_BYTES, cudaMemcpyHostToDevice);
+
+    //kernel invocation
+    add_kernel<<<ceil(N/1024), 1024>>>(device_a,device_b,device_result, N);
+
+    cudaMemcpy(result, device_result, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+    cudaFree(device_a);
+    cudaFree(device_b);
+    cudaFree(device_result);
+    cudaDeviceReset();
+    return result;
 }
 
 // Configure the dynamic memory to initialize
 // the variables a, b and result as pointers.
 
-void onHost() {
+void onDevice() {
     int N = 10;
 
     // -:YOUR CODE HERE:-
+    const int ARRAY_SIZE = 10240000;
+    const int ARRAY_BYTES = ARRAY_SIZE * sizeof(int);
+    int *a, *b, *result;
 
-    for (int i = 0; i < N; i++) {
+    a = (int*)malloc(ARRAY_BYTES);
+    b = (int*)malloc(ARRAY_BYTES);
+    result = (int*)malloc(ARRAY_BYTES);
+
+    for (int i = 0; i < ARRAY_SIZE; i++) {
         a[i] = -i;
         b[i] = i * i;
         result[i] = 0;
     }
 
-    add(a, b, result, N);
+    add(a, b, result, ARRAY_SIZE);
+
 
     for (int i = 0; i < N; i++) {
         assert(a[i] + b[i] == result[i]);
     }
 
     printf("-: successful execution :-\n");
-
+   
     // -:YOUR CODE HERE:-
 }
 
 int main() {
-    onHost();
+    onDevice();
     return 0;
 }
