109b8b6ebda780aa99e54f19bbfec6614b0beafd
lxy <577086410@qq.com>
2024-08-03 16:19:01 +0800

    finished 05_Matrix_manipulation/Source_code/03matrices_additionv2_P/03matrices_additionv2.cu

--- 05_Matrix_manipulation/Source_code/03matrices_additionv2_P/03matrices_additionv2.cu
+++ 05_Matrix_manipulation/Source_code/03matrices_additionv2_P/03matrices_additionv2.cu
@@ -1,11 +1,28 @@
 #include <assert.h>
+#include <cstddef>
 #include <stdio.h>
 #include "Error.h"
 
 #define N 500
 
+__device__ int calcIndex(int x, int y) {
+    return y * N + x;
+}
+
 __global__ void additionMatricesKernel(int* d_a, int* d_b, int* d_c) {
     // -:YOUR CODE HERE:-
+    size_t y = threadIdx.y + blockIdx.y * blockDim.y;
+    size_t Xstride = gridDim.x * blockDim.x;
+    size_t Ystride = gridDim.y * blockDim.y;
+    while(y < N) {
+        size_t x = threadIdx.x + blockIdx.x * blockDim.x;
+        while(x < N) {
+            size_t idx = calcIndex(x, y);
+            d_c[idx] = d_a[idx] + d_b[idx];
+            x += Xstride;
+        }
+        y += Ystride;
+    }
 }
 
 void onDevice(int h_a[][N], int h_b[][N], int h_c[][N]) {
@@ -16,9 +33,13 @@ void onDevice(int h_a[][N], int h_b[][N], int h_c[][N]) {
 
     // allocate  memory on the GPU
     // -:YOUR CODE HERE:-
-
+    cudaMalloc((void**)&d_a, ARRAY_BYTES);
+    cudaMalloc((void**)&d_b, ARRAY_BYTES);
+    cudaMalloc((void**)&d_c, ARRAY_BYTES);
     // copy data from CPU the GPU
     // -:YOUR CODE HERE:-
+    cudaMemcpy(d_a, h_a, ARRAY_BYTES, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, h_b, ARRAY_BYTES, cudaMemcpyHostToDevice);
 
     // execution configuration
     dim3 GridBlocks(4, 4);
@@ -30,9 +51,12 @@ void onDevice(int h_a[][N], int h_b[][N], int h_c[][N]) {
 
     // copy data back from the GPU to the CPU
     // -:YOUR CODE HERE:-
-
+    cudaMemcpy(h_c, d_c, ARRAY_BYTES, cudaMemcpyDeviceToHost);
     // free GPU memory
     // -:YOUR CODE HERE:-
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
 }
 
 void test(int h_a[][N], int h_b[][N], int h_c[][N]) {
